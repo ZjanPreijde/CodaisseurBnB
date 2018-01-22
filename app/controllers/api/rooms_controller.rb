4ce860bb7ab2b61de915d6f0f82952aa4cdf188a
ZjanPreijde <j.preijde2@upcmail.nl>
2018-01-22 16:14:02 +0100

    Add API rooms show and create, started on update/destroy

--- app/controllers/api/rooms_controller.rb
+++ app/controllers/api/rooms_controller.rb
@@ -1,8 +1,61 @@
 # app/controllers/api/rooms_controller.rb
 
 class Api::RoomsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
     render status: 200,
-     json: { rooms: Room.all }.to_json
+           json: { rooms: Room.all }.to_json
+  end
+
+  def show
+    room = Room.find(params[:id])
+
+    render status: 200,
+           json: { room: room }.to_json
+  end
+
+  def create
+    room = Room.new(room_params)
+
+    if room.save
+      render status: 201,
+             json: { message: 'Room successfully created', room: room }.to_json
+    else
+      render status: 422,
+             json: { errors: room.errors }.to_json
+    end
+  end
+
+  def update
+    room = Room.find(params[:id])
+    if room.update
+      render status: 201,
+             json: { message: 'Room successfully updated', room: room }.to_json
+    else
+      render status: 422,
+             json: { errors: room.errors }.to_json
+    end
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    if room.destroy
+      render status: 201,
+             json: { message: 'Room successfully deleted', room: room }.to_json
+    else
+      render status: 422,
+             json: { errors: room.errors }.to_json
+    end
+  end
+
+  private
+
+  def room_params
+    params
+      .require(:room)
+      .permit(:home_type, :room_type, :accommodate, :bedroom_count,
+              :bathroom_count, :listing_name, :description, :address,
+              :has_tv, :has_kitchen, :has_airco, :has_heating, :has_internet,
+              :price, :active, theme_ids: [])
   end
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApplicationController
-  before_action :set_room, only: [:show, :edit, :update]
+  before_action :set_room, only: %i[show edit update]
   before_action :authenticate_user!, except: [:show]
 
   def index
@@ -18,29 +18,34 @@ class RoomsController < ApplicationController
     @room = current_user.rooms.build(room_params)
 
     if @room.save
-      redirect_to @room, notice: "Room successfully created"
+      redirect_to @room, notice: 'Room successfully created'
     else
       render :new
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   def update
     if @room.update(room_params)
-      redirect_to @room, notice: "Room successfully updated"
+      redirect_to @room, notice: 'Room successfully updated'
     else
       render :edit
     end
   end
 
   private
-    def set_room
-      @room = Room.find(params[:id])
-    end
 
-    def room_params
-      params.require(:room).permit(:home_type, :room_type, :accommodate, :bedroom_count, :bathroom_count, :listing_name, :description, :address, :has_tv, :has_kitchen, :has_airco, :has_heating, :has_internet, :price, :active, theme_ids: [])
-    end
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params
+      .require(:room)
+      .permit(:home_type, :room_type, :accommodate, :bedroom_count,
+              :bathroom_count, :listing_name, :description, :address,
+              :has_tv, :has_kitchen, :has_airco, :has_heating, :has_internet,
+              :price, :active, theme_ids: [])
+  end
 end
